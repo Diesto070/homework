60314d813087fb6b3890f61b843d4c8d344e8d84
Ivan <diestoapp@gmail.com>
2025-12-16 13:11:23 +0300

    homework fix project and finish homework 12.2

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,16 @@
-from typing import Dict, List
-
-dict_list = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
-
-def filter_by_state(dict_list: List[Dict[str, str]], state: str = 'EXECUTED') -> list[Dict[str, str]]:
-    """"Функция принимает список словарей и опционально значение для ключа state(по умолчанию 'EXECUTED').
-        Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
-        соответствует указанному значению."""
-    return list((item for item in dict_list if item.get('state') == state))
+def filter_by_state(lst: list, state: str = "EXECUTED") -> list:
+    """
+    Функция принимает state опционально и возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению.
+    """
+    new_list = [i for i in lst if i.get("state") == state]
+    return new_list
 
 
-def sort_by_date(dict_list: List[Dict[str, str]]) -> list[Dict[str, str]]:
-    """Функция сортировки даты"""
-    return sorted(dict_list, key=lambda item: item['date'], reverse=True)
+def sort_by_date(lst: list, key_sort: bool = True) -> list:
+    """
+    Функция принимает список словарей и необязательный параметр
+    и возвращает новый список, отсортированный по дате (date).
+    """
+    new_list = sorted(lst, key=lambda x: x.get("date"), reverse=key_sort)
+    return new_list
